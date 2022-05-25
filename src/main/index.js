468c88bfc7feaeb4bf6e20163e5ad54b162731b5
Uheeking <wert1847@naver.com>
2022-05-25 14:08:31 +0900

    antd 기본 적용

--- src/main/index.js
+++ src/main/index.js
@@ -21,12 +21,7 @@ function MainPage() {
   }, []);
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" />
-        </div>
-      </div>
-      <div id="body">
+      
         <div id="banner">
           <img src="images/banners/banner1.png" />
         </div>
@@ -35,7 +30,7 @@ function MainPage() {
           {products.map(function (product, index) {
             return (
               <div className="product-card">
-                <Link className="product-link" to={`/product/${index}`}>
+                <Link className="product-link" to={`/products/${product.id}`}>
                   <div>
                     <img className="product-img" src={product.imageUrl} />
                   </div>
@@ -55,8 +50,6 @@ function MainPage() {
             );
           })}
         </div>
-      </div>
-      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import {useParams} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "./index.css";
 
 function ProductPage() {
   // 도메인에서 받은 id를 파라미터로 저장
-  const {id} = useParams;
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://5d7190ed-4b8a-4d00-be90-a13732db212f.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  console.log(product);
+  
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  
   return (
-    <h1>
-      상세페이지화면 {id}
-    </h1>
-  )
+    <div>
+      <div id="image-box">
+        <img src={"/"+ product.imageUrl} />
+        {/* <img src={`/${product.imageUrl}`} /> */}
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seleer}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ProductPage
+export default ProductPage;
