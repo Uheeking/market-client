3eb9b4e2c2d7a1a8b478908601d5e7201c007a95
Uheeking <wert1847@naver.com>
2022-05-26 15:55:39 +0900

    api_url 수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production'? 'https://market-client-uheeking.vercel.app' : 'http//localhost:8080'
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://market-server-uheeking.herokuapp.com"
+    : "http//localhost:8080";
