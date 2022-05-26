5df6c9fa1978e9a8583fda34c086e2150966131e
Uheeking <wert1847@naver.com>
2022-05-26 15:34:38 +0900

    api 주소 변경

---
+++ src/config/constants.js
@@ -0,0 +1,1 @@
+export const API_URL = process.env.NODE_ENV === 'production'? 'https://market-client-uheeking.vercel.app' : 'http//localhost:8080'
--- src/main/index.js
+++ src/main/index.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import "./index.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { API_URL } from "../config/constants";
+import { Carousel } from "antd";
+
+dayjs.extend(relativeTime);
 
 function MainPage() {
   const [products, setProducts] = useState([]);
+  const [banners, setBanners] = useState([]);
   useEffect(() => {
     axios
-      .get(
-        "https://5d7190ed-4b8a-4d00-be90-a13732db212f.mock.pstmn.io/products"
-      )
+      .get(`${API_URL}/products`)
       .then(function (result) {
         console.log(result);
         const products = result.data.products;
@@ -18,25 +23,46 @@ function MainPage() {
       .catch(function (error) {
         console.error("에러 발생 :", error);
       });
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러 발생 : ", error);
+      });
   }, []);
   return (
     <div>
-      
-        <div id="banner">
-          <img src="images/banners/banner1.png" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
-                <Link className="product-link" to={`/products/${product.id}`}>
-                  <div>
-                    <img className="product-img" src={product.imageUrl} />
-                  </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageUrl}`} />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
+      <h1 id="product-headline">판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              {product.soldout === 1 && <div className="product-blur" />}
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img
+                    className="product-img"
+                    src={`${API_URL}/${product.imageUrl}`}
+                  />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-footer">
                     <div className="product-seller">
                       <img
                         className="product-avatar"
@@ -44,12 +70,16 @@ function MainPage() {
                       />
                       <span>{product.seller}</span>
                     </div>
+                    <span className="product-date">
+                      {dayjs(product.createdAt).fromNow()}
+                    </span>
                   </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
